82e38fc9ee457ef629d41e9e93c0943491f29ebe
v0lk0daf <volkov-a-s@yandex.ru>
2019-12-22 20:49:48 +0500

    Update Happy_tickets.py

--- Happy_tickets.py
+++ Happy_tickets.py
@@ -3,7 +3,7 @@ count = 0
 while x < 1000000 :
     if (x%1000000//100000 + x%100000//10000 + x%10000//1000
         == x%1000//100 + x%100//10 + x%10) :
-        print (str(x).zfill(6))
+        # print (str(x).zfill(6)) 
         count += 1
         x += 1
     else:
