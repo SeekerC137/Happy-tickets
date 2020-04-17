08183ed177e7925cec14e3b4139d95d8243bc12a
v0lk0daf <volkov-a-s@yandex.ru>
2020-04-17 18:08:58 +0500

    Update Happy_tickets.py

--- Happy_tickets.py
+++ Happy_tickets.py
@@ -3,10 +3,10 @@ count = 0
 while x < 1000000 :
     if (x%1000000//100000 + x%100000//10000 + x%10000//1000
         == x%1000//100 + x%100//10 + x%10) :
-        # print (str(x).zfill(6)) 
+        # print (str(x).zfill(6)) # uncomment to print all happy tickets
         count += 1
         x += 1
     else:
         x += 1
-print ("Итого счастливых билетов: ", str(count))
+print ("Amount of happy tickets in packing: ", str(count))
 
